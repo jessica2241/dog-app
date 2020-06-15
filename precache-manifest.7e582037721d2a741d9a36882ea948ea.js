0e9ea90943d5832dd533df0cf6b12c225e53b633
root <root@v282158.hosted-by-vdsina.ru>
2020-06-16 00:09:16 +0300

    Updates

---
+++ precache-manifest.7e582037721d2a741d9a36882ea948ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9e271d3ae37558710b168edc598e5c",
+    "url": "/cat-app/index.html"
+  },
+  {
+    "revision": "fce3b9fdbc4a292fd660",
+    "url": "/cat-app/static/css/2.47f050ad.chunk.css"
+  },
+  {
+    "revision": "83f95708ef7d7aecba00",
+    "url": "/cat-app/static/css/main.3e7ae0cb.chunk.css"
+  },
+  {
+    "revision": "fce3b9fdbc4a292fd660",
+    "url": "/cat-app/static/js/2.a4006ec3.chunk.js"
+  },
+  {
+    "revision": "4c0abbc2f365a9dae473263e58562193",
+    "url": "/cat-app/static/js/2.a4006ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f95708ef7d7aecba00",
+    "url": "/cat-app/static/js/main.b7dde5f1.chunk.js"
+  },
+  {
+    "revision": "00e037b3b4355fdb3d90",
+    "url": "/cat-app/static/js/runtime-main.1311ae35.js"
+  }
+]);
